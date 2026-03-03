32231eea10d60373c5ff25c5c7e34336eb660fa0
Romel Diaz <rodira.codes@gmail.com>
2026-03-03 08:51:27 -0500

    service constansts

---
+++ src/Constants/X12Service.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Romeldev\SusaludX12\Constants;
+
+class X12Service
+{
+    const REG_AFI_270       = 'RegAfi270';
+    const CON_ASE_270       = 'ConAse270';
+    const REG_AFI_271       = 'RegAfi271';
+    const CON_COD_271       = 'ConCod271';
+    const CON_MED_271       = 'ConMed271';
+    const CON_NOM_271       = 'ConNom271';
+    const SOL_AUT_271       = 'SolAut271';
+    const IN_271_CON_DTAD   = 'In271ConDtad';
+    const IN_271_CON_OBS    = 'In271ConObs';
+    const IN_271_CON_PROC   = 'In271ConProc';
+    const IN_271_RES_DERIVA = 'In271ResDeriva';
+    const IN_271_RES_SCTR   = 'In271ResSctr';
+    const LOG_ACRE_INSERT   = 'LogAcreInsert271';
+    const IN_278_SOL_CG     = 'In278SolCG';
+    const IN_278_RES_CG     = 'In278ResCG';
+    const CON_ENT_VINC_278  = 'ConEntVinc278';
+    const RES_ENT_VINC_278  = 'ResEntVinc278';
+    const IN_997_RES_AUT    = 'In997ResAut';
+}
--- src/Services/SusaludX12Manager.php
+++ src/Services/SusaludX12Manager.php
@@ -2,6 +2,8 @@
 
 namespace Romeldev\SusaludX12\Services;
 
+use Romeldev\SusaludX12\Constants\X12Service;
+
 use InvalidArgumentException;
 
 /**
@@ -14,25 +16,46 @@ class SusaludX12Manager
 
     /** @var array Mapa de nombres a clases de servicio */
     private static $serviceMap = [
-        'RegAfi270'     => RegAfi270Service::class,
-        'ConAse270'     => ConAse270Service::class,
-        'RegAfi271'     => RegAfi271Service::class,
-        'ConCod271'     => ConCod271Service::class,
-        'ConMed271'     => ConMed271Service::class,
-        'ConNom271'     => ConNom271Service::class,
-        'SolAut271'     => SolAut271Service::class,
-        'In271ConDtad'  => In271ConDtadService::class,
-        'In271ConObs'   => In271ConObsService::class,
-        'In271ConProc'  => In271ConProcService::class,
-        'In271ResDeriva' => In271ResDerivaService::class,
-        'In271ResSctr'  => In271ResSctrService::class,
-        'LogAcreInsert271' => LogAcreInsert271Service::class,
-        'In278SolCG'    => In278SolCGService::class,
-        'In278ResCG'    => In278ResCGService::class,
-        'ConEntVinc278' => ConEntVinc278Service::class,
-        'ResEntVinc278' => ResEntVinc278Service::class,
-        'In997ResAut'   => In997ResAutService::class,
+        X12Service::REG_AFI_270       => RegAfi270Service::class,
+        X12Service::CON_ASE_270       => ConAse270Service::class,
+        X12Service::REG_AFI_271       => RegAfi271Service::class,
+        X12Service::CON_COD_271       => ConCod271Service::class,
+        X12Service::CON_MED_271       => ConMed271Service::class,
+        X12Service::CON_NOM_271       => ConNom271Service::class,
+        X12Service::SOL_AUT_271       => SolAut271Service::class,
+        X12Service::IN_271_CON_DTAD   => In271ConDtadService::class,
+        X12Service::IN_271_CON_OBS    => In271ConObsService::class,
+        X12Service::IN_271_CON_PROC   => In271ConProcService::class,
+        X12Service::IN_271_RES_DERIVA => In271ResDerivaService::class,
+        X12Service::IN_271_RES_SCTR   => In271ResSctrService::class,
+        X12Service::LOG_ACRE_INSERT   => LogAcreInsert271Service::class,
+        X12Service::IN_278_SOL_CG     => In278SolCGService::class,
+        X12Service::IN_278_RES_CG     => In278ResCGService::class,
+        X12Service::CON_ENT_VINC_278  => ConEntVinc278Service::class,
+        X12Service::RES_ENT_VINC_278  => ResEntVinc278Service::class,
+        X12Service::IN_997_RES_AUT    => In997ResAutService::class,
     ];
+
+    // private static $serviceMap = [
+    //     'RegAfi270'     => RegAfi270Service::class,
+    //     'ConAse270'     => ConAse270Service::class,
+    //     'RegAfi271'     => RegAfi271Service::class,
+    //     'ConCod271'     => ConCod271Service::class,
+    //     'ConMed271'     => ConMed271Service::class,
+    //     'ConNom271'     => ConNom271Service::class,
+    //     'SolAut271'     => SolAut271Service::class,
+    //     'In271ConDtad'  => In271ConDtadService::class,
+    //     'In271ConObs'   => In271ConObsService::class,
+    //     'In271ConProc'  => In271ConProcService::class,
+    //     'In271ResDeriva' => In271ResDerivaService::class,
+    //     'In271ResSctr'  => In271ResSctrService::class,
+    //     'LogAcreInsert271' => LogAcreInsert271Service::class,
+    //     'In278SolCG'    => In278SolCGService::class,
+    //     'In278ResCG'    => In278ResCGService::class,
+    //     'ConEntVinc278' => ConEntVinc278Service::class,
+    //     'ResEntVinc278' => ResEntVinc278Service::class,
+    //     'In997ResAut'   => In997ResAutService::class,
+    // ];
 
     /**
      * Obtiene un servicio por nombre.
